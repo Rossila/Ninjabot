7c0fb32a779a91d64f9df98992ed875873435f9a
Amanda Tu <tu.amanda3@gmail.com>
2012-10-25 01:30:41 -0700

    fixed functions for zero slope and infinite slope

--- pathfinding.py
+++ pathfinding.py
@@ -13,6 +13,7 @@ l_red = cv.RGB(250, 200, 200)
 black = cv.RGB(100, 100, 100)
 
 # define radii
+# make the obstacle_radius + rover_width 2/sqrt(2) times larger than it needs to be 
 ball_radius = 18
 obstacle_radius = 28
 rover_width = 50
@@ -102,57 +103,62 @@ def intersect(ln1_start, ln1_end, ln2_start, ln2_end):
 		ccw(ln1_start,ln1_end,ln2_start) != ccw(ln1_start,ln1_end,ln2_end)
 
 def checkIntersections(bot_loc, bot_dest, obstacles, intersections):
-	# find the slope of the robot's current tragectory
-	def getSlope(bot_loc, bot_dest):
-		if bot_dest[0] - bot_loc[0] == 0 or bot_dest[1] - bot_loc[1] == 0:
-			return None
-		slope = fractions.Fraction(bot_dest[1] - bot_loc[1],bot_dest[0] - bot_loc[0])
-		print "the slope is: ", slope
-		return slope
 
-	def findConstant(slope, distance):
-		# (ax)/(ay) find c s.t. (ax)^2 + (ay)^2 = distance^2
+	def findConstants(bot_loc, bot_dest, distance):
+		# (ax)/(ay) find a s.t. (ax)^2 + (ay)^2 = distance^2
 		# ax and ay will be the x and y displacement
+		x_dspl = 0
+		y_dspl = 0
 
-		a = math.sqrt(pow(distance, 2)/float(pow(slope.numerator,2) + pow(slope.denominator,2)))
-		#print a
-		return a
+		diff_y = bot_dest[0] - bot_loc[0]
+		diff_x = - bot_dest[1] + bot_loc[1]
+
+		#we're done! close enough, don't do anything more
+		if diff_x <= 10 and diff_x >= -10 and diff_y <= 10 and diff_y >= -10: 
+			pass
+		elif diff_x == 0:
+			x_dspl = 0
+			y_dspl = distance
+		elif diff_y == 0:
+			x_dspl = distance
+			y_dspl = 0
+		else: 
+			a = math.sqrt(pow(distance, 2)/float(pow(diff_x,2) + pow(diff_y,2)))
+			x_dspl = a * diff_x
+			y_dspl = a * diff_y
+		return x_dspl, y_dspl
 
 
 	# returns the two end points of a parallel line segment (@distance) away from the original line
 	def getParallelLine(ln1_start, ln1_end, distance, dir):
 		ln2_start = (0,0)
 		ln2_end = (0,0)
-		slope = getSlope(bot_loc, bot_dest)
-		if slope == None:
-			return None
-		perpSlope = -1/slope
-		#print perpSlope
 
-		a = findConstant(perpSlope, distance)
+		x_dspl, y_dspl = findConstants(bot_loc, bot_dest, distance)
 		
 		if dir == "bottom":
-			a = a * -1
-		ln2_start = (int(ln1_start[0] + a*perpSlope.denominator), int(ln1_start[1] + a*perpSlope.numerator))
-		ln2_end = (int(ln1_end[0] + a*perpSlope.denominator), int(ln1_end[1] + a*perpSlope.numerator))
+			x_dspl = -1 * x_dspl
+			y_dspl = -1 * y_dspl
+		ln2_start = (int(ln1_start[0] + x_dspl), int(ln1_start[1] + y_dspl))
+		ln2_end = (int(ln1_end[0] + x_dspl), int(ln1_end[1] + y_dspl))
 
 		return ln2_start, ln2_end
 
 	def checkObstacles(bot_loc, bot_dest, obstacles, intersections):
-		slope = getSlope(bot_loc, bot_dest)
-		if slope == None:
-			return intersections
-		perpSlope = -1/slope
+		#slope = getSlope(bot_loc, bot_dest)
+		#if slope == None:
+		#	return intersections
+		#perpSlope = -1/slope
 		
 		ln2_start_top, ln2_end_top = getParallelLine(bot_loc, bot_dest, 50, "top")
 		ln2_start_bot, ln2_end_bot = getParallelLine(bot_loc, bot_dest, 50, "bottom")
 		
 		#draw_line(ln2_start_top, ln2_end_top, d_red)
 		#draw_line(ln2_start_bot, ln2_end_bot, d_red)
-		a = findConstant(perpSlope, obstacle_radius + rover_width)
+		x_dspl, y_dspl = findConstants(bot_loc, bot_dest, obstacle_radius + rover_width)
 		for obstacle in obstacles:
-			obs_proj1 = (int(obstacle[0] + a*perpSlope.denominator), int(obstacle[1] + a*perpSlope.numerator))
-			obs_proj2 = (int(obstacle[0] + -1*a*perpSlope.denominator), int(obstacle[1] + -1*a*perpSlope.numerator))
+			obs_proj1 = (int(obstacle[0] + x_dspl), int(obstacle[1] + y_dspl))
+			obs_proj2 = (int(obstacle[0] + -1*x_dspl), int(obstacle[1] + -1*y_dspl))
 			draw_line(obs_proj1, obs_proj2, d_blue)
 			print "obstacle", obstacle
 			print "obs_proj1", obs_proj1
@@ -169,11 +175,6 @@ def checkIntersections(bot_loc, bot_dest, obstacles, intersections):
 	
 	return checkObstacles(bot_loc, bot_dest, obstacles, intersections)
 
-
-
-
-
-
 # Initialize Coordinates
 bot_loc = (image.shape[1]/2, 0)
 bot_dir = 90
@@ -189,7 +190,7 @@ for obstacle in obstacles:
 
 
 index = find_closest_ball(balls, bot_loc)
-index = 3
+#index = 2
 draw_line(bot_loc, balls[index], d_red)
 angle = line_angle(bot_loc, balls[index])
 
