549e8a4bad78379b620d76cf85ecac5ee68c10d8
Amanda Tu <tu.amanda3@gmail.com>
2012-11-08 01:12:49 -0800

    new working algorithm that travels along tangent lines

--- pathfinding.py
+++ pathfinding.py
@@ -12,7 +12,8 @@ d_blue = cv.RGB(55, 65, 150)
 l_red = cv.RGB(250, 200, 200)
 black = cv.RGB(100, 100, 100)
 d_purple = cv.RGB(150, 55, 150)
-
+# a list of ranges of x & y to indicate previously travelled paths
+travelled_paths = []
 # define radii
 # make the obstacle_radius + rover_width 2/sqrt(2) times larger than it needs to be 
 ball_radius = 18
@@ -178,8 +179,6 @@ def checkIntersections(bot_loc, bot_dest, obstacles, intersections):
 			obs_proj1 = (int(obstacle[0] + x_dspl), int(obstacle[1] + y_dspl))
 			obs_proj2 = (int(obstacle[0] + -1*x_dspl), int(obstacle[1] + -1*y_dspl))
 			draw_line(obs_proj1, obs_proj2, d_blue)
-			print "obstacle", obstacle
-			print "obs_proj1", obs_proj1
 			if intersect(bot_loc, bot_dest, obs_proj1, obs_proj2) or distance_between_points(bot_dest, obstacle) < obstacle_radius + rover_width:
 			#	print "intersection found at", obs_proj1
 			#	intersections.append(index)
@@ -196,6 +195,13 @@ def getPOI(bot_loc, bot_dest, obstacle, POI):
 	def getSlope(theta):
 		slope = math.sin(theta)/math.cos(theta)
 		return slope
+
+	def getEndPt(startPoint, secondPoint, distance):
+		ratio = (distance + 10)/distance_between_points(startPoint, secondPoint)
+		diffx = secondPoint[0] - startPoint[0]
+		diffy = secondPoint[1] - startPoint[1]
+
+		return (secondPoint[0] + diffx * ratio, secondPoint[1] + diffy * ratio)
 
 	def getTangent(bot_loc, obstacle):
 		hypotenuse = distance_between_points(bot_loc, obstacle)
@@ -236,24 +242,32 @@ def getPOI(bot_loc, bot_dest, obstacle, POI):
 	y1 = bslope1 * x1 + bdispl1
 	if int(distance_between_points((x1, y1), obstacle)) - obstacle_radius - rover_width <= 2 and x1 > rover_width and y1 > rover_width:
 		draw_circle(4, int(x1), int(y1), black)
+		(x1, y1) = getEndPt(bot_loc, (x1, y1), obstacle_radius + rover_width)
+		#draw_circle(4, int(x1), int(y1), d_purple)
 		POI.append((int(x1), int(y1)))
 
 	x2 = (bdispl2 - adispl2)/(aslope2 - bslope2)
 	y2 = bslope2 * x2 + bdispl2
 	if int(distance_between_points((x2, y2), obstacle)) - obstacle_radius - rover_width <=2 and x2 > rover_width and y2 > rover_width:
 		draw_circle(4, int(x2), int(y2), black)
+		(x2, y2) = getEndPt(bot_loc, (x2, y2), obstacle_radius + rover_width)
+		#draw_circle(4, int(x1), int(y1), d_purple)
 		POI.append((int(x2), int(y2)))
 
 	x3 = (bdispl1 - adispl2)/(aslope2 - bslope1)
 	y3 = bslope1 * x3 + bdispl1
 	if int(distance_between_points((x3, y3), obstacle)) - obstacle_radius - rover_width <=2 and x3 > rover_width and y3 > rover_width:
 		draw_circle(4, int(x3), int(y3), black)
+		(x3, y3) = getEndPt(bot_loc, (x3, y3), obstacle_radius + rover_width)
+		#draw_circle(4, int(x1), int(y1), d_purple)
 		POI.append((int(x3), int(y3)))
 
 	x4 = (bdispl2 - adispl1)/(aslope1 - bslope2)
 	y4 = bslope2 * x4 + bdispl2
 	if int(distance_between_points((x4, y4), obstacle)) - obstacle_radius - rover_width <=2 and x4 > rover_width and y4 > rover_width:
 		draw_circle(4, int(x4), int(y4), black)
+		(x4, y4) = getEndPt(bot_loc, (x4, y4), obstacle_radius + rover_width)
+		#draw_circle(4, int(x1), int(y1), d_purple)
 		POI.append((int(x4), int(y4)))
 
 	return POI
@@ -295,47 +309,71 @@ for obstacle in obstacles:
 index = find_closest_ball(balls, bot_loc)
 
 # FOR TESTING PURPOSES, MODIFY THIS INDEX TO CHANGE WHICH BALL TO SEARCH FOR
-index = 3
+index = 2
 draw_line(bot_loc, balls[index], d_red)
 angle = line_angle(bot_loc, balls[index])
 
 print "The angle is: ", angle
 
-intersections = []
-POI = []
-checked_obs = {0: False, 1: False, 2: False, 3: False}
-next_pt = (0,0)
+def findPath(last_pt, bot_loc, next_pt, obstacles):
+	intersections = []
+	POI = []
+	checked_obs = {0: False, 1: False, 2: False, 3: False}
+	
 
-intersections = checkIntersections(bot_loc, balls[index], obstacles, intersections)
-print intersections
+	intersections = checkIntersections(bot_loc, balls[index], obstacles, intersections)
+	print intersections
 
-if len(intersections) == 0:
-	next_pt = balls[index]
+	if len(intersections) == 0:
+		next_pt = balls[index]
 
-for intersection in intersections:
-	checked_obs[intersection] = True
-	POI = getPOI(bot_loc, balls[index], obstacles[intersection], POI)
-
-print "POI: ", POI
-
-#while next_pt == (0,0) and len(POI) > 0:
-#	print "popped again"
-test_pt = POI.pop(0)
-intersections = []
-intersections = checkIntersections(bot_loc, test_pt, obstacles, intersections)
-if len(intersections) == 0:
-	next_pt = test_pt
-	print "We're done! The next point is: ", next_pt
-else:
 	for intersection in intersections:
-	    if checked_obs[intersection] == False:
-			print "another obstacle was encountered, adding more POIs"
-			checked_obs[intersection] == False
-			POI = getPOI(bot_loc, balls[index], obstacles[intersection], POI)
+		checked_obs[intersection] = True
+		POI = getPOI(bot_loc, balls[index], obstacles[intersection], POI)
 
-draw_circle(4, next_pt[0], next_pt[1], d_red)
+	print "POI: ", POI
 
-robotTravel(bot_dir, bot_loc, next_pt)
+	while next_pt == (0,0) and len(POI) > 0:
+		print "popped again"
+		test_pt = POI.pop(0)
+		for path in travelled_paths:
+			if test_pt[0] > path[0][0] and test_pt[0] < path[0][1] and test_pt[1] > path[1][0] and test_pt[1] < path[1][1]:
+				print "THIS POINT HAS ALREADY BEEN VISITED"
+				test_pt = (0,0)
+				break # this point has already been visited, try another one
+		if test_pt == (0,0):
+			continue
+		intersections = []
+		intersections = checkIntersections(bot_loc, test_pt, obstacles, intersections)
+		if len(intersections) == 0:
+			next_pt = test_pt
+			print "We're done! The next point is: ", next_pt
+		else:
+			for intersection in intersections:
+			    if checked_obs[intersection] == False:
+					print "another obstacle was encountered, adding more POIs"
+					checked_obs[intersection] == False
+					POI = getPOI(bot_loc, balls[index], obstacles[intersection], POI)
+
+	draw_circle(4, next_pt[0], next_pt[1], d_red)
+	robotTravel(bot_dir, bot_loc, next_pt)
+	return next_pt
+
+# returns a boolean indicating whether or not the robot has reached it's destination
+def check_dest(bot_loc, bot_dest):
+	return distance_between_points(bot_loc, bot_dest) < rover_width
+
+
+next_pt = (0,0)
+last_pt = (0,0)
+
+while not check_dest(bot_loc,balls[index]):
+	next_pt = findPath(last_pt, bot_loc, next_pt, obstacles)
+	travelled_paths.append(((min(bot_loc[0],next_pt[0])-rover_width/2, max(bot_loc[0],next_pt[0]) + rover_width/2), \
+		(min(bot_loc[1],next_pt[1]) - rover_width/2, max(bot_loc[1],next_pt[1]) + rover_width/2)))
+	last_pt = bot_loc
+	bot_loc = next_pt
+	next_pt = (0, 0)
 
 
 
